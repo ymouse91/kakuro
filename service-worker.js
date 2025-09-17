2aff21b9c773cc17871dc297e1c227f7f76ff84d
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-09-17 13:25:12 +0300

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'tavupeli-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/pwa_icon-192.png',
+  '/pwa_icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
