f7606980abcdafed2e3d103c0de3d8e8d290aa76
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-09-17 13:27:28 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
 const CACHE_NAME = 'tavupeli-v1';
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/manifest.json',
-  '/pwa_icon-192.png',
-  '/pwa_icon-512.png'
+  './',
+  './index.html',
+  './manifest.json',
+  './pwa_icon_180.png',
+  './pwa_icon-192.png',
+  './pwa_icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
