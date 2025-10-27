93bae5b235ad4794b84d60f61afc26dcf8248eef
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-10-27 19:45:52 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'kakuro-v1.20';
+const CACHE_NAME = 'kakuro-v1.21';
 const ASSETS = [
   './',
   './index.html',
   './ohje.html',
   './manifest.json',
   './pwa_icon_192.png',
-  './pwa_icon_512.png',
+  './pwa_icon_512.png'
 ];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
