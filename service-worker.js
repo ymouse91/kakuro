fbd809f7887e201ba826440edbebf599c4f138b3
ymouse91 <35890980+ymouse91@users.noreply.github.com>
2025-10-27 16:50:28 +0000

    Update service-worker.js
    Sw

--- service-worker.js
+++ service-worker.js
@@ -4,8 +4,8 @@ const ASSETS = [
   './index.html',
   './ohje.html',
   './manifest.json',
-  './pwa_icon-192.png',
-  './pwa_icon-512.png'
+  './pwa_icon_192.png',
+  './pwa_icon_512.png',
 ];
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));
